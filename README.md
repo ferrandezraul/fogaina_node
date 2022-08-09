1563681f020800390d3800d7104c3a0fd5ee6a01
Raul Ferrandez <ferrandezraul@gmail.com>
2022-08-10 01:12:29 +0200

    Initial nodejs app with express and ejs

---
+++ README.md
@@ -0,0 +1,11 @@
+# Node App 
+
+Starts nodemon behind the scenes:
+```
+npm start
+```
+
+Call eslint to detect issues:
+```
+npm run lint
+```
---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.listen(port, () => {
+    console.log(`Running on port ${port}`);
+})
