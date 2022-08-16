2d52781bb70259b5f1945e216b532c8d8e34218d
Raul Ferrandez <ferrandezraul@gmail.com>
2022-08-16 15:05:18 +0200

    WIP

--- postsInstagram.js
+++ postsInstagram.js
@@ -3,5 +3,21 @@ exports.postsInstagram = {
     "1": {
         url: "https://www.instagram.com/p/Cfdt9LqN9a-/?utm_source=ig_embed&amp;utm_campaign=loading",
         description: "Brioix"
+    },
+    "2": {
+        url: "https://www.instagram.com/p/Cg3l2CZthOs/?utm_source=ig_embed&amp;utm_campaign=loading",
+        description: "Espelta semi"
+    },
+    "3": {
+        url: "https://www.instagram.com/p/CgWnvurtha5/?utm_source=ig_embed&amp;utm_campaign=loading",
+        description: "Focaccia tomate"
+    },
+    "4": {
+        url: "https://www.instagram.com/p/CfyBA3xNRZS/?utm_source=ig_embed&amp;utm_campaign=loading",
+        description: "Empanadillas"
+    },
+    "5": {
+        url: "https://www.instagram.com/p/Cf4iJdbtB_j/?utm_source=ig_embed&amp;utm_campaign=loading",
+        description: "Croissants"
     }
 }
