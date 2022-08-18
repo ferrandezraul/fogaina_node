3114c0e6dfdfcd1f9f68f808e41a87b013f826ad
Raul Ferrandez <ferrandezraul@gmail.com>
2022-08-18 23:21:35 +0200

    WIP

--- pans.js
+++ pans.js
@@ -3,116 +3,139 @@ exports.pans = {
     "soca": {
         name: "Soca",
         description: "Un pa fet amb una barreja de farines integrals de varietats antigues de blat (espelta, kamut, xeixa ...) cultivats per petits productors, format de motlle i fet amb massa mare. Hem aconseguit aquest pa gràcies a la tècnica del Soaker: grans de blat i d'arròs integral cuits que aporten un extra d'esponjositat i humitat a la molla. Adquiriu sabor i salut a un preu d'escàndol!",
-        image: "images/pans/soca.jpg"
+        image: "images/pans/soca.jpg",
+        image2: "images/pans/soca2.jpg"
     },
     "pages": {
         name: "Pa de pagés",
         description: "Sens dubte el pa que més ens va costar definir-ne la fórmula. Sembla mentida: un pa tan senzill a primera vista i, precisament, per això, ens va donar molta feina, perquè volíem fer un pa senzill que no fos vulgar. Farina semiblanca de molí de pedra i una massa mare natural, que el doten d'una ànima làctica i suau, fugint del què és insípid. El forn amb solera de pedra és el company ideal per aconseguir una bona fogassa. Pa de pagés de 600gr i de 1Kg.",
-        image: "images/pans/padepages.jpg"
+        image: "images/pans/padepages.jpg",
+        image2: "images/pans/soca2.jpg"
     },
     "rustic": {
         name: "Rústic",
         description: "Motivats per aconseguir una fogassa similar al pa de pagès, però amb la intenció de proveir-lo de més fibra i sabor, i un millor alveolat, sense ajuda de llevat, només amb una massa mare jove, i afegint-t'hi farines integrals de blat i de sègol. Vam haver d'anar pujant la hidratació de la massa, que després es va convertir tècnicament en el repte per als nous forners  de la Fogaina. El forn a  265 ºC i molta humitat són l'últim esglaó per aconseguir una bonica i daurada fogassa.",
-        image: "images/pans/rustic.jpg"
+        image: "images/pans/rustic.jpg",
+        image2: "images/pans/soca2.jpg"
     },
     "pigat": {
         name: "Pigat",
         description: "Agafem la massa del Rústic i hi afegim una barreja de llavors torrades (lli, sèsam, flocs de civada, pipes de gira-sol )i deixades en remull... Més gust, si és possible, a la nostra massa rústica!",
-        image: "images/pans/pigat.jpg"
+        image: "images/pans/pigat.jpg",
+        image2: "images/pans/soca2.jpg"
     },
     "tinosell": {
         name: "Tinosell",
         description: "I encara motivats per oferir més diversitat panarra a les nostres lleixes, ens vam treure de la màniga aquest panot ideal per a esmorzars i berenars, o per combinar si vols amb un bon tast de formatges. A la nostra massa rústica li afegim fruita seca (panses, nous i orellanes ) i la fiquem dins d'un motlle. L'oli de les anous tenyeix d'un preciós morat la molla d'aquest pa tan especial.",
-        image: "images/pans/tinosell.jpg"
+        image: "images/pans/tinosell.jpg",
+        image2: "images/pans/soca2.jpg"
     },
     "espelta": {
         name: "Espelta",
         description: "Per als esportistes, puristes, integristes, espeltatians... No hi ha millor plaer que un pa 100 % espelta i quasi integral, amb la seva dosi de massa mare d'espelta, i amenitzat amb unes pipes de gira-sol torrades. Molt bon gust i llarga conservació.",
-        image: "images/pans/espelta.jpg"
+        image: "images/pans/espelta.jpg",
+        image2: "images/pans/soca2.jpg"
     },
     "semiespelta": {
         name: "Semi integral d’espelta",
         description: "Per aquells que voleu menjar sa, però no us tiran els pans integrals. Fogassa 100% de farina d’espelta. Pel tipus de gra que és l’espelta no es pot aconseguir mai una farina blanca, però aquesta és la d’extracció més blanca possible i de proximitat, quasi Km 0. Farina del pla de l’estany.",
-        image: "images/pans/semi-integral-espelta.jpg"
+        image: "images/pans/semi-integral-espelta.jpg",
+        image2: "images/pans/soca2.jpg"
     },
     "coca": {
         name: "Coca de forner",
         description: "Una fleca amb un bon forn no es pot permetre no coure unes coques de forner. El motiu és el ritme de cocció que ens ofereix el nostre forn. Pugem el forn fins a quasi 300 ºC . El procés de format, estirat i repòs és gradual començant pel pre-format i fins a l'últim estirat a la mateixa pala, un bon raig d'oli d'oliva, empolsinada de sucre i cap a dins! Quan surt, una ruixada amb anís per cristal·litzar el sucre... No us podeu imaginar la fragància que omple tot l'obrador.",
-        image: "images/pans/cocaforner.jpg"
+        image: "images/pans/cocaforner.jpg",
+        image2: "images/pans/soca2.jpg"
     },
     "pacoca": {
         name: "Pa de coca",
         description: "Pel tipus de forn que teníem ens era difícil oferir barres tradicionals. Així que aquest és el nostre producte que més s'adaptava a l'hora de l'entrepà. És una mena de xapata allargada, alveolada i de crosta fina. El toc d'oli d'oliva i el de gira-sol, tots dos d'altíssima qualitat, li donen un sabor i una textura particulars. Ara, ja fem barres, però qualsevol deixa de fer el pa de coca amb la d’aficionats que té!",
-        image: "images/pans/padecoca.jpg"
+        image: "images/pans/padecoca.jpg",
+        image2: "images/pans/soca2.jpg"
     },
     "brotnegre": {
         name: "Brot negre",
         description: "Si tenim un pa que desvetlla passions confrontades aquest és el Brot. Un panot  de sègol integral, una mica de blat integral i massa mare de sègol. La cultura del sègol a la Mediterrània és escassa, per això li donem aquesta oportunitat, a més de tastar una cosa ben diferent i aromàtica, menjareu pura salut i tindreu a les vostres mans un pa que dura molts dies. Acompanyeu-lo de formatges forts, anxoves, patés, mantega o melmelada amarga ...",
-        image: "images/pans/brotnegre.jpg"
+        image: "images/pans/brotnegre.jpg",
+        image2: "images/pans/soca2.jpg"
     },
     "bembo": {
         name: "Bembó",
         description: "Per què hem de renunciar als pans de motllo blancs i esponjosos que ofereixen les marques industrials? Si a més aconseguim que siguin més bons i saludables ... motllo blanc i tendre, fet amb llet vegetal. Farina de blat T80. Massa mare i preferment. I a sobre, vegà!! Ja que me substituit la mantega pel tang Zhong, una tècnica coreana que li dona aquesta amorositat i també el gust dolcet.",
-        image: "images/pans/bembo.jpg"
+        image: "images/pans/bembo.jpg",
+        image2: "images/pans/soca2.jpg"
     },
     "fajol": {
         name: "Fajol",
         description: "Una mica obligats per la demanda, i limitats per les 'traces', finalment ens vam decidir a fer un pa 'sense gluten', que per les condicions del nostre obrador, ens impedeix de certificar-lo per a celíacs. La nostra intenció des del primer moment era aconseguir un molt bon pa sense gluten. Hem après unes quantes coses sobre mètodes i nous ingredients. Així que esperem estar a l'alçada i que el gaudiu.",
-        image: "images/pans/fajol.jpg"
+        image: "images/pans/fajol.jpg",
+        image2: "images/pans/soca2.jpg"
     },
     "croscat": {
         name: "Croscat",
         description: "Fogassa petita de farina d'espelta. Per als puristes, que cuiden la salut però volen fogasses. Un pa 100 % espelta i quasi integral, amb la seva dosi de massa mare d'espelta, i amenitzat amb unes pipes de gira-sol torrades. Molt bon gust i llarga conservació.",
-        image: "images/pans/croscat.jpg"
+        image: "images/pans/croscat.jpg",
+        image2: "images/pans/soca2.jpg"
     },
     "fogassa": {
         name: "Fogassa Integral",
         description: "Farina de blat 100% integral y farina de blat antic fermentat només amb massa mare.",
-        image: "images/pans/fogassa_integral.jpg"
+        image: "images/pans/fogassa_integral.jpg",
+        image2: "images/pans/soca2.jpg"
     },
     "rugbrot": {
         name: "Rugbrot",
         description: "El rugbrød o pa de sègol danès és el més comunament consumit a Dinamarca. Mescla de farines de blat integral, farina de sègol integral, massa mare de sègol i llavors. Espectacular, una explosió de sabors, i a més, gràcies a les llavors dura molts dies fresc. El rugbrød amb mantega és la base essencial d'un dels més famosos sandvitxos oberts danesos, el smørrebrød.",
-        image: "images/pans/rougbrot.jpg"
+        image: "images/pans/rougbrot.jpg",
+        image2: "images/pans/soca2.jpg"
     },
     "monococum": {
         name: "Monococum",
         description: "Pa semiintegral fermentat només amb massa mare, amb poca acidesa i sabor que recorda a llegums, té molt poc contingut de gluten i alt contingut de fibra cosa que el fa molt més digerible. Fet amb Blat monococcum, que é la primera varietat de blat domesticada per l'ésser humà fa més de 10.000 anys i que donaria origen als blats moderns. ",
-        image: "images/pans/monococum.jpg"
+        image: "images/pans/monococum.jpg",
+        image2: "images/pans/soca2.jpg"
     },
     "olives": {
         name: "Pa d’Olives",
         description: "Un dels especials de dissabte, Fet amb massa de farina semi integral, farina de sègol i Olives  de kalamata, que li donen un sabor espectacular. ",
-        image: "images/pans/olives.jpg"
+        image: "images/pans/olives.jpg",
+        image2: "images/pans/soca2.jpg"
     },
     "nous": {
         name: "Pa de nous",
         description: "Un dels especials de dissabte, fet amb massa de farina semi integral, farina de sègol i  nous torrades.",
-        image: "images/pans/nous.jpg"
+        image: "images/pans/nous.jpg",
+        image2: "images/pans/soca2.jpg"
     },
     "koulouri": {
         name: "Koulouri",
         description: "Pa tradicional grec, de farina blanca, i semi integral, amb tahina, mel i llavors de sèsam. Aquests panets són una delícia... s'assemblen als Bagels... són bonics i aromàtics",
-        image: "images/pans/Kouluris.jpg"
+        image: "images/pans/Kouluris.jpg",
+        image2: "images/pans/soca2.jpg"
     },
     "tano": {
         name: "Tano",
         description: "Panet blanc i tendre, fet amb llet vegetal. Farina de blat T80. Massa mare i preferment. Ideal per als entrepans de la mainada.",
-        image: "images/pans/tanos.jpg"
+        image: "images/pans/tanos.jpg",
+        image2: "images/pans/soca2.jpg"
     },
     "llonguet": {
         name: "Llonguet",
         description: "Pa tradicional català, fet amb farina blanca, massa mare, una mica de llevat i fermentació llarga. és un panet allargat amb una única grenya transversal a la part superior. És típic de les Illes Balears i de Catalunya. fruit d'un llarg treball de pastat. Això fa que sigui una peça de pa delicada, però molt apreciada.",
-        image: "images/pans/llonguet.jpg"
+        image: "images/pans/llonguet.jpg",
+        image2: "images/pans/soca2.jpg"
     },
     "xapates": {
         name: "Xapates",
         description: "",
-        image: "images/pans/xapates.jpg"
+        image: "images/pans/xapates.jpg",
+        image2: "images/pans/soca2.jpg"
     },
     "baguette": {
         name: "Baguette",
         description: "",
-        image: "images/pans/baguette.jpg"
+        image: "images/pans/baguette.jpg",
+        image2: "images/pans/soca2.jpg"
     }
 }
